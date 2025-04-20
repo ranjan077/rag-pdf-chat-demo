fe486b8f123e3e7889a455353f529fbed760a06e
Ranjan Nagaraju <ranjan.un@gmail.com>
2025-04-20 13:33:59 +0530

    added next js client app with cleark authentication

---
+++ client/rag-pdf-chat-demo-app/src/app/components/SignIn.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import { SignIn, useSignIn } from "@clerk/nextjs";
+
+const SignInSection: React.FC = () => {
+  const { isLoaded } = useSignIn();
+  return (
+    <div className="flex items-center justify-center h-screen">
+      {isLoaded ? <SignIn /> : "loading..."}
+    </div>
+  );
+};
+
+export default SignInSection;
