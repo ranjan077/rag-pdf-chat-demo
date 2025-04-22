e6598082b237b03cbdfd834f04fabbd993523106
Ranjan Nagaraju <ranjan.un@gmail.com>
2025-04-22 21:51:34 +0530

    fix sending files to server upload api

--- client/src/app/api/upload.ts
+++ client/src/app/api/upload.ts
@@ -1,8 +1,12 @@
 const uploadFile = async (formData) => {
   try {
+    const urlEncoded = new URLSearchParams(formData);
     const response = await fetch("http://localhost:8000/upload/pdf", {
       method: "POST",
-      body: formData,
+      body: urlEncoded,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
     });
 
     if (!response.ok) {
